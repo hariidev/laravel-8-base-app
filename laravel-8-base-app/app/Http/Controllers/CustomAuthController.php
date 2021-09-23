ce3f4db0c0c7b8acd804fa191a6fa1267eede5b8
Hari Sugirthan <hsugeen14@gmail.com>
2021-09-23 17:20:01 +0530

    login function added

--- laravel-8-base-app/app/Http/Controllers/CustomAuthController.php
+++ laravel-8-base-app/app/Http/Controllers/CustomAuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 
 class CustomAuthController extends Controller
@@ -43,5 +44,31 @@ class CustomAuthController extends Controller
             'password' => Hash::make($data['password'])
         ]);
     }
+
+    public function customLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+   
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')
+                        ->withSuccess('Signed in');
+        }
+  
+        return redirect("login")->withSuccess('Login details are not valid');
+    }
+
+    public function dashboard()
+    {
+        if(Auth::check()){
+            return view('dashboard');
+        }
+  
+        return redirect("login")->withSuccess('You are not allowed to access');
+    }
+
 
 }
