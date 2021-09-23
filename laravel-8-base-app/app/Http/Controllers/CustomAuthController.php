1c084c28456ae8014d50d2529653a594422d1d05
Hari Sugirthan <hsugeen14@gmail.com>
2021-09-23 17:18:38 +0530

    Registation function added

--- laravel-8-base-app/app/Http/Controllers/CustomAuthController.php
+++ laravel-8-base-app/app/Http/Controllers/CustomAuthController.php
@@ -3,12 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
 
 class CustomAuthController extends Controller
 {
     public function index()
     {
         return view('auth.login');
-    }  
-      
+    }
+
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+
+
+    public function customRegistration(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $data = $request->all();
+        $check = $this->create($data);
+
+        return redirect("dashboard")->withSuccess('You have signed-in');
+    }
+
+
+    public function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+    }
+
 }
