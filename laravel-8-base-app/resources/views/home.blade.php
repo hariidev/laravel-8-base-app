067fb9a9c9c15ddc66afe3720ac86aa2b11559f1
Hari Sugirthan <hsugeen14@gmail.com>
2021-10-07 16:33:00 +0530

    title changed

--- laravel-8-base-app/resources/views/home.blade.php
+++ laravel-8-base-app/resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Laravel Pagination</title>
+    <title>Employee table</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 </head>
