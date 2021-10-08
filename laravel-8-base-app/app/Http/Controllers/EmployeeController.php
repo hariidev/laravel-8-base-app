117c599ab5d20fcf9e923b554399cf6f1c4b77c4
Hari Sugirthan <hsugeen14@gmail.com>
2021-10-08 16:07:10 +0530

    function added

--- laravel-8-base-app/app/Http/Controllers/EmployeeController.php
+++ laravel-8-base-app/app/Http/Controllers/EmployeeController.php
@@ -12,4 +12,10 @@ class EmployeeController extends Controller
         $employees = Employee::paginate(50);
         return view('home', compact('employees'));
     }
+
+    public function setData()
+    {
+        $employees = Employee::paginate(50);
+        return view('home', compact('employees'));
+    }
 }
