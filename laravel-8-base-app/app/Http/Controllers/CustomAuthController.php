78e1b9dbe05557edbaa3d599db9acdd272ae6f83
Hari Sugirthan <hsugeen14@gmail.com>
2021-10-13 16:15:43 +0530

    function added

--- laravel-8-base-app/app/Http/Controllers/CustomAuthController.php
+++ laravel-8-base-app/app/Http/Controllers/CustomAuthController.php
@@ -62,7 +62,15 @@ class CustomAuthController extends Controller
         return redirect("login")->withSuccess('Login details are not valid');
     }
 
-    
+    public function dashboard()
+    {
+        if(Auth::check()){
+            return view('dashboard');
+        }
+  
+        return redirect("login")->withSuccess('You are not allowed to access');
+    }
+
     
     public function signOut() {
         Session::flush();
