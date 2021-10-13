3feecefa982220d2ce75aac78f0ba2a0f3f0c968
Hari Sugirthan <hsugeen14@gmail.com>
2021-10-13 16:15:26 +0530

    function removed

--- laravel-8-base-app/app/Http/Controllers/CustomAuthController.php
+++ laravel-8-base-app/app/Http/Controllers/CustomAuthController.php
@@ -62,15 +62,7 @@ class CustomAuthController extends Controller
         return redirect("login")->withSuccess('Login details are not valid');
     }
 
-    public function dashboard()
-    {
-        if(Auth::check()){
-            return view('dashboard');
-        }
-  
-        return redirect("login")->withSuccess('You are not allowed to access');
-    }
-
+    
     
     public function signOut() {
         Session::flush();
