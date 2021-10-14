87da7bbb766b641598a547d71b538c71a7777125
Hari Sugirthan <hsugeen14@gmail.com>
2021-10-14 15:43:17 +0530

    add index function

--- laravel-8-base-app/app/Http/Controllers/ProductController.php
+++ laravel-8-base-app/app/Http/Controllers/ProductController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::all()->toArray();
+        return array_reverse($products);
+    }
 }
