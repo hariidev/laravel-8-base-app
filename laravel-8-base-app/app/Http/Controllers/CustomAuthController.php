93fb7e530d26ff709e74ccb46e9b205f219484da
Hari Sugirthan <hsugeen14@gmail.com>
2021-10-27 17:27:00 +0530

    Update CustomAuthController.php

--- laravel-8-base-app/app/Http/Controllers/CustomAuthController.php
+++ laravel-8-base-app/app/Http/Controllers/CustomAuthController.php
@@ -22,20 +22,7 @@ class CustomAuthController extends Controller
     }
 
 
-    public function customRegistration(Request $request)
-    {
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|min:6',
-        ]);
-
-        $data = $request->all();
-        $check = $this->create($data);
-
-        return redirect("dashboard")->withSuccess('You have signed-in');
-    }
-
+ 
 
     public function create(array $data)
     {
