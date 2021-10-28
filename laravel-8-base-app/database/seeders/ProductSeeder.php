aa8943b929166accb2d57a754c05c4a8ef250695
Hari Sugirthan <hsugeen14@gmail.com>
2021-10-28 12:39:48 +0530

    Product DB seeder added

---
+++ laravel-8-base-app/database/seeders/ProductSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+use Faker\Factory as Faker;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+    	foreach (range(1,200) as $index) {
+            Product::insert([
+                'name' => $faker->firstname,
+                'detail' => $faker->address,
+            ]);
+        }
+    }
+}
