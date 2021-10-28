0713c840744bacd01c03a23f0863f785442b6528
Hari Sugirthan <hsugeen14@gmail.com>
2021-10-28 16:09:53 +0530

    Employee DB seeder added

---
+++ laravel-8-base-app/database/seeders/EmployeeSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Employee;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        foreach (range(1, 500) as $index) {
+            Employee::insert([
+                'firstname' => $faker->firstname,
+                'lastname' => $faker->lastname,
+                'email' => $faker->email,
+                'dob' => $faker->date($format = 'D-m-y', $max = '2021', $min = '2010')
+            ]);
+        }
+    }
+}
