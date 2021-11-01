9bdcff2ebeabf12206f4cc40912f4c3817e661c1
Hari Sugirthan <hsugeen14@gmail.com>
2021-11-01 17:04:50 +0530

    Update CustomAuthController.php

--- laravel-8-base-app/app/Http/Controllers/CustomAuthController.php
+++ laravel-8-base-app/app/Http/Controllers/CustomAuthController.php
@@ -29,7 +29,8 @@ class CustomAuthController extends Controller
             'password' => 'required|min:6',
         ]);
 
-      
+        $data = $request->all();
+        $check = $this->create($data);
 
         return redirect("dashboard")->withSuccess('You have signed-in');
     }
