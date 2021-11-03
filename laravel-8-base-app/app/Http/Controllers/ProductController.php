9ac5b40a82043464d78d7c47ef2fd00a334a4549
Hari Sugirthan <hsugeen14@gmail.com>
2021-11-03 17:23:30 +0530

    Update ProductController.php

--- laravel-8-base-app/app/Http/Controllers/ProductController.php
+++ laravel-8-base-app/app/Http/Controllers/ProductController.php
@@ -9,16 +9,13 @@ class ProductController extends Controller
 {
     public function index()
     {
-       
+        $products = Product::all()->toArray();
+        return array_reverse($products);
     }
 
     public function store(Request $request)
     {
-        $product = new Product([
-            'name' => $request->input('name'),
-            'detail' => $request->input('detail')
-        ]);
-        $product->save();
+       
 
         return response()->json('Product created!');
     }
