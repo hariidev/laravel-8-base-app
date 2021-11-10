64fdbf0569fc60b57e2013246beb6e679dbd3101
Hari Sugirthan <hsugeen14@gmail.com>
2021-11-10 18:06:31 +0530

    Update CustomAuthController.php

--- laravel-8-base-app/app/Http/Controllers/CustomAuthController.php
+++ laravel-8-base-app/app/Http/Controllers/CustomAuthController.php
@@ -37,11 +37,7 @@ class CustomAuthController extends Controller
 
     public function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password'])
-        ]);
+       
     }
 
     public function customLogin(Request $request)
