78f204dd9bd9107ba18772af8c3b816f560bd3c2
Hari Sugirthan <hsugeen14@gmail.com>
2022-01-06 12:54:29 +0530

    Update AppServiceProvider.php

--- laravel-8-base-app/app/Providers/AppServiceProvider.php
+++ laravel-8-base-app/app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Schema::defaultStringLength(191);
-        // Paginator::useBootstrap();
+        Schema::defaultStringLength(191);
+        Paginator::useBootstrap();
     }
 }
