c9ddacbe4d8874224cde2732e1564d3dfdea31df
Hari Sugirthan <hsugeen14@gmail.com>
2022-01-13 10:43:36 +0530

    Update 2021_10_14_100302_create_products_table.php

--- laravel-8-base-app/database/migrations/2021_10_14_100302_create_products_table.php
+++ laravel-8-base-app/database/migrations/2021_10_14_100302_create_products_table.php
@@ -13,12 +13,12 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        // Schema::create('products', function (Blueprint $table) {
-        //     $table->id();
-        //     $table->string('name');
-        //     $table->text('detail');
-        //     $table->timestamps();
-        // });
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('detail');
+            $table->timestamps();
+        });
     }
 
     /**
