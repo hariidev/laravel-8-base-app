744461501de71c5e8037af04b0bef49d2b79fc0d
Hari Sugirthan <hsugeen14@gmail.com>
2022-01-16 21:38:36 +0530

    Update manager.php

--- laravel-8-base-app/routes/manager.php
+++ laravel-8-base-app/routes/manager.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return('Welcome to manager user routes.');
-    });
+// Route::get('/', function () {
+//     return('Welcome to manager user routes.');
+//     });
 
 
 ?>
