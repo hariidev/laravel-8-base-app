07e080fea2dc575cc938e5a7844c09d4cfed5236
Hari Sugirthan <hsugeen14@gmail.com>
2022-01-20 16:43:00 +0530

    LiveWire counter added to home

--- laravel-8-base-app/resources/views/home.blade.php
+++ laravel-8-base-app/resources/views/home.blade.php
@@ -7,6 +7,7 @@
     <title>Employee table</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    @livewireStyles
 </head>
 
 <body>
@@ -37,6 +38,9 @@
             {!! $employees->links() !!}
         </div>
     </div>
+    <livewire:counter />
+
+    @livewireScripts
 </body>
 
 </html>
