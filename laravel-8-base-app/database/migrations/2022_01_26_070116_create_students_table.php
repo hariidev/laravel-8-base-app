2fbac4bd1068f45cfdc749881d380f7691838993
Hari Sugirthan <hsugeen14@gmail.com>
2022-01-26 12:57:12 +0530

    student table properties added

--- laravel-8-base-app/database/migrations/2022_01_26_070116_create_students_table.php
+++ laravel-8-base-app/database/migrations/2022_01_26_070116_create_students_table.php
@@ -15,6 +15,9 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->string('email');
+            $table->string('mobile');
             $table->timestamps();
         });
     }
