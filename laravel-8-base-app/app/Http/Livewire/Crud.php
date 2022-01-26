ccee89ec76ba27db46cbcd3fcd09c0e3ac9fbbec
Hari Sugirthan <hsugeen14@gmail.com>
2022-01-26 13:26:11 +0530

    Store() added - Crud

--- laravel-8-base-app/app/Http/Livewire/Crud.php
+++ laravel-8-base-app/app/Http/Livewire/Crud.php
@@ -3,11 +3,32 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Student;
 
 class Crud extends Component
 {
     public function render()
     {
         return view('livewire.crud');
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'mobile' => 'required',
+        ]);
+
+        Student::updateOrCreate(['id' => $this->student_id], [
+            'name' => $this->name,
+            'email' => $this->email,
+            'mobile' => $this->mobile,
+        ]);
+
+        session()->flash('message', $this->student_id ? 'Student updated.' : 'Student created.');
+
+        $this->closeModalPopover();
+        $this->resetCreateForm();
     }
 }
