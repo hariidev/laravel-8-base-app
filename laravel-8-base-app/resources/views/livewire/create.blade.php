88e2c9d81b39bb515688c9a3f642b45623091add
Hari Sugirthan <hsugeen14@gmail.com>
2022-01-31 17:53:01 +0530

    Update create.blade.php

--- laravel-8-base-app/resources/views/livewire/create.blade.php
+++ laravel-8-base-app/resources/views/livewire/create.blade.php
@@ -44,10 +44,10 @@
                         </button>
                     </span>
                     <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
-                        <button wire:click="closeModalPopover()" type="button"
+                        <!-- <button wire:click="closeModalPopover()" type="button"
                             class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-bold text-gray-700 shadow-sm hover:text-gray-700 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                             Close
-                        </button>
+                        </button> -->
                     </span>
                 </div>
             </form>
