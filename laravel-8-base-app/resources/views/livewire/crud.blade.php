bc0a6a4ea52d6450b18ca6f666e24ca862c38df7
Hari Sugirthan <hsugeen14@gmail.com>
2022-02-02 17:58:17 +0530

    Update crud.blade.php

--- laravel-8-base-app/resources/views/livewire/crud.blade.php
+++ laravel-8-base-app/resources/views/livewire/crud.blade.php
@@ -32,7 +32,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($students as $students)
+                    @foreach($students as $student)
                     <tr>
                         <td class="border px-4 py-2">{{ $student->id }}</td>
                         <td class="border px-4 py-2">{{ $student->name }}</td>
